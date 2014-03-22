0c4040a6085b361bfe9713ea124c95e7cbb04520
Alexander Nemtarev <devnull@localhost>
2014-03-22 14:48:05 +0400

    extjs 3.4 для тестирования клиентского рендеринга

---
+++ src/m3_ext/static/client_render/static/extjs/locale/ux-lang-ru.js
@@ -0,0 +1,12 @@
+if(Ext.ux.tree.TreeGridSorter){
+  Ext.apply(Ext.ux.tree.TreeGridSorter.prototype, {
+    sortAscText  : "Сортировать по возрастанию",
+    sortDescText : "Сортировать по убыванию"
+  });
+}
+
+if(Ext.ux.tree.TreeGrid){
+  Ext.apply(Ext.ux.tree.TreeGrid.prototype, {
+    columnsText  : "Столбцы"
+  });
+}
