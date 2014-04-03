36d3c1ef41e98d763201499c6bfeaf116286916b
Telepenin Nikolay <telepenin@bars-open.ru>
2014-04-03 15:06:42 +0400

    Build js. Контрольный!

--- src/m3_ext/static/m3/js/m3-debug.js
+++ src/m3_ext/static/m3/js/m3-debug.js
@@ -3541,9 +3541,7 @@ Ext.onReady(Ext.ux.Lightbox.init, Ext.ux.Lightbox);
  * Содержит общие функции вызываемые из разных частей
  */
 Ext.QuickTips.init();
-Ext.m3.ExtJS_version = (function() {
-    return Ext.version.split(".").slice(0, 2).join(".");
-})();
+
 /**
  * Чтобы ie и прочие не правильные браузеры, где нет console не падали
  */
