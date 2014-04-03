82487a019e017c7fd374568ab999616536b2b16c
Борисов Кирилл <borisov@bars-open.ru>
2014-04-03 11:39:35 +0400

    BaseExtListWindow адаптирован (mock-грид).

--- src/m3_ext/ui/windows/lists.py
+++ src/m3_ext/ui/windows/lists.py
@@ -13,43 +13,39 @@ class BaseExtListWindow(BaseExtWindow):
     """
     Базовое окно со списком записей.
     """
+    _xtype = BaseExtWindow._xtype
+
+    js_attrs = BaseExtWindow.js_attrs.extend(
+        'layout', 'width', 'height', 'items', 'maximizable'
+    )
+    
     def __init__(self, *args, **kwargs):
         super(BaseExtListWindow, self).__init__(*args, **kwargs)
+        self.setdefault('width', 800)
+        self.setdefault('width', 600)
+        self.setdefault('maximized', True)
+        self.setdefault('layout', 'border')
 
-        #self.template = 'ext-windows/ext-window.js'
-
-        self.layout = 'border'
-        self.width = 800
-        self.height = 600
-        self.maximized = True
-
-        # грид, который будем все для нас делать
-        self.grid = panels.ExtObjectGrid(region='center')
-        self.items.append(self.grid)
-
-    # Данный код должен находится в базовом классе, но т.к. не вcе шаблоны
-    # переведены на новый рендеринг, остается пока в каждом
-    def render(self):
-        assert getattr(self, '_ext_name'), (
-            'Class %s is not define "_ext_name"' % self.__class__.__name__
-        )
-
-        self.pre_render()
-
-        try:
-            self.render_base_config()
-            self.render_params()
-        except UnicodeDecodeError:
-            raise Exception('Some attribute is not unicode')
-        except Exception as msg:
-            raise Exception(msg)
-
-        base_config = self._get_config_str()
-        params = self._get_params_str()
-        res = '%(ext_name)s({%(base_config)s},{%(params)s})' % {
-            'ext_name': self._ext_name,
-            'base_config': base_config,
-            'params': params
+        # FIXME: заменить на полноценный ExtObjectGrid, как только его адаптируют
+        self.grid = {
+            'xtype': 'panel',
+            'layout': 'fit',
+            'region': 'center',
+            'items': [{
+                'xtype': 'grid',
+                'columns': [
+                    {'header': u'Первая колонка', 'width': 400, 'data_index': 'first'},
+                    {'header': u'Вторая колонка', 'width': 400, 'data_index': 'second'}
+                ],
+                'layout': 'fit',
+                'itemId': 'grid',
+                'store': {
+                        'xtype': 'jsonstore',
+                        'fields': [
+                            {'type': 'string', 'name': 'first'},
+                            {'type': 'string', 'name': 'second'}
+                        ]
+                    }
+                }]
         }
-
-        return 'new %s' % res if not self._is_function_render else res
+        self.items.append(self.grid)
