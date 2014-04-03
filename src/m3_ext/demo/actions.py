e18c4eb71cfdd6679d1e11e50ecf07dd695d08ff
Борисов Кирилл <borisov@bars-open.ru>
2014-04-03 17:10:33 +0400

    Переведен ExtEditWindow и добавлена демка для него.

--- src/m3_ext/demo/actions.py
+++ src/m3_ext/demo/actions.py
@@ -149,3 +149,56 @@ class ListWindowAction(UIAction):
             {'first': u'Первая', 'second': u'Вторая'},
         ]
         return res
+
+@Pack.register
+class EditWindowAction(UIAction):
+    """
+    Пример оконного экшна со списком (гридом) внутри.
+    """
+    title = u'Окно с формой'
+
+    def get_js(self, request, context):
+        """
+        Метод должен вернуть в виде строки js-код для окна
+        """
+        return """function(w, d) { 
+            var form = w.find('itemId', 'form')[0];
+            var field = w.find('itemId', 'edit-field-id')[0];
+            form.buttons[0].on('click', function(b, e){
+                alert(field.getValue());
+            });
+            form.buttons[1].on('click', function(b, e){
+                w.close();
+            });
+        }"""
+
+    def get_ui(self, request, context):
+        win = ext.ExtEditWindow(
+            title=u'Окно с формой',
+            layout='form',
+            width=250,
+            height=150)
+
+        # FIXME: Заменить на полноценный ExtForm c полями после конвертации
+        win.form = {
+            'labelWidth': 75,
+            'width': 230,
+            'height': 100,
+            'frame': True,
+            'itemId': 'form',
+            'xtype': 'form',            
+            'defaultType': 'textfield',
+            'items': [
+                {
+                    'fieldLabel': u'Поле ввода',
+                    'xtype': 'textfield',
+                    'itemId': 'edit-field-id'
+                },
+            ],
+            'buttons': [    
+                ext.ExtButton(text=u'Отобразить'),
+                ext.ExtButton(text=u'Отмена')
+            ]
+        }
+        win.items.append(win.form)
+        return win
--- src/m3_ext/ui/windows/edit_window.py
+++ src/m3_ext/ui/windows/edit_window.py
@@ -14,62 +14,8 @@ from m3_ext.ui.windows.base import ExtWindowRenderer
 class ExtEditWindow(BaseExtWindow):
     """Базовый класс окна редактрирования"""
 
+    _xtype = 'm3-window'
+
     def __init__(self, *args, **kwargs):
         super(ExtEditWindow, self).__init__(*args, **kwargs)
-        self.__form = None
-        self.data_url = None  # адрес для загрузки данных формы
-        self._ext_name = 'Ext.m3.EditWindow'
-        self.renderer = ExtWindowRenderer('ext-script/ext-editwindowscript.js')
-        self.init_component(*args, **kwargs)
-
-    @property
-    def handler_beforesubmit(self):
-        return self._listeners.get('beforesubmit')
-
-    @handler_beforesubmit.setter
-    def handler_beforesubmit(self, function):
-        self._listeners['beforesubmit'] = function
-
-    @property
-    def form(self):
-        return self.__form
-
-    @form.setter
-    def form(self, value):
-        # self.items = [value,] -- Если с этим окном используется всегда форма,
-        # то небходимо вставить эту строку
-        self.items.append(value)
-        self.__form = value
-
-    def render_params(self):
-        super(ExtEditWindow, self).render_params()
-        if self.form:
-            if isinstance(self.form, ExtForm):
-                self._put_params_value(
-                    'form',
-                    {'id': self.form.client_id, 'url': self.form.url})
-        self._put_params_value('dataUrl', self.data_url)
-
-    # Данный код должен находится в базовом классе, но т.к. не вcе шаблоны
-    # переведены на новый рендеринг, остается пока в каждом
-    def render(self):
-        assert getattr(self, '_ext_name'), \
-            'Class %s is not define "_ext_name"' % (self.__class__.__name__,)
-
-        self.pre_render()
-
-        try:
-            self.render_base_config()
-            self.render_params()
-        except TemplateSyntaxError as msg:
-            raise Exception(msg)
-
-        base_config = self._get_config_str()
-        params = self._get_params_str()
-        res = '%(ext_name)s({%(base_config)s},{%(params)s})' % {
-            'ext_name': self._ext_name,
-            'base_config': base_config,
-            'params': params
-        }
-
-        return 'new %s' % res if not self._is_function_render else res
+        self.form = None
