7e4c23470230a49200d237e3f9adf083d2c48911
Telepenin Nikolay <telepenin@bars-open.ru>
2014-04-05 15:02:42 +0400

    Переработанные филды

--- src/m3_ext/static/m3/js/ext-extensions/AdvancedDateField.js
+++ src/m3_ext/static/m3/js/ext-extensions/AdvancedDateField.js
@@ -3,112 +3,98 @@
  * Добавлена кнопа установки текущий даты
  */
 Ext.m3.AdvancedDataField = Ext.extend(Ext.form.DateField, {
-	constructor: function(baseConfig, params){
-        params = baseConfig.params || params || {};
+    constructor: function (baseConfig) {
 
-		// Базовый конфиг для тригеров
-		this.baseTriggers = [
-			{
-				iconCls: 'x-form-date-trigger'
-				,handler: null
-				,hide:null
-			},
-			{
-				iconCls: 'x-form-current-date-trigger'
-				,handler: null
-				,hide:null
-			}
-		];
+        // Базовый конфиг для тригеров
+        this.baseTriggers = [
+            {
+                iconCls: 'x-form-date-trigger', handler: null, hide: null
+            },
+            {
+                iconCls: 'x-form-current-date-trigger', handler: null, hide: null
+            }
+        ];
 
-		this.hideTriggerToday = false;
-
-
-		if (params.hideTriggerToday) {
-			this.hideTriggerToday = true;
-		}
-
-		Ext.m3.AdvancedDataField.superclass.constructor.call(this, baseConfig);
-	}
-	,initComponent: function(){
-		Ext.m3.AdvancedDataField.superclass.initComponent.call(this);
+        Ext.m3.AdvancedDataField.superclass.constructor.call(this, baseConfig);
+    }, initComponent: function () {
+        Ext.m3.AdvancedDataField.superclass.initComponent.call(this);
 
         this.triggerConfig = {
-            tag:'span', cls:'x-form-twin-triggers', cn:[]};
+            tag: 'span', cls: 'x-form-twin-triggers', cn: []};
 
-		Ext.each(this.baseTriggers, function(item, index, all){
-			this.triggerConfig.cn.push(
-				{tag: "img", src: Ext.BLANK_IMAGE_URL, cls: "x-form-trigger " + item.iconCls}
-			);
-		}, this);
+        Ext.each(this.baseTriggers, function (item, index, all) {
+            this.triggerConfig.cn.push(
+                {tag: "img", src: Ext.BLANK_IMAGE_URL, cls: "x-form-trigger " + item.iconCls}
+            );
+        }, this);
 
-		this.initBaseTrigger()
-	},
-	initTrigger : function(){
+        this.initBaseTrigger()
+    },
+    initTrigger: function () {
 
         var ts = this.trigger.select('.x-form-trigger', true);
         var triggerField = this;
-        ts.each(function(t, all, index){
+        ts.each(function (t, all, index) {
 
-            var triggerIndex = 'Trigger'+(index+1);
-            t.hide = function(){
+            var triggerIndex = 'Trigger' + (index + 1);
+            t.hide = function () {
                 var w = triggerField.wrap.getWidth();
                 this.dom.style.display = 'none';
-                triggerField.el.setWidth(w-triggerField.trigger.getWidth());
+                triggerField.el.setWidth(w - triggerField.trigger.getWidth());
                 this['hidden' + triggerIndex] = true;
             };
-            t.show = function(){
+            t.show = function () {
                 var w = triggerField.wrap.getWidth();
                 this.dom.style.display = '';
-                triggerField.el.setWidth(w-triggerField.trigger.getWidth());
+                triggerField.el.setWidth(w - triggerField.trigger.getWidth());
                 this['hidden' + triggerIndex] = false;
             };
 
-            if( this.baseTriggers[index].hide ){
+            if (this.baseTriggers[index].hide) {
                 t.dom.style.display = 'none';
                 this['hidden' + triggerIndex] = true;
             }
-            this.mon(t, 'click', this.baseTriggers[index].handler, this, {preventDefault:true});
+            this.mon(t, 'click', this.baseTriggers[index].handler, this, {preventDefault: true});
             t.addClassOnOver('x-form-trigger-over');
             t.addClassOnClick('x-form-trigger-click');
         }, this);
 
         this.triggers = ts.elements;
-    }
-	,initBaseTrigger: function(){
-		this.baseTriggers[0].handler = this.onTriggerClick;
-		this.baseTriggers[1].handler = function(){
+    }, initBaseTrigger: function () {
+        this.baseTriggers[0].handler = this.onTriggerClick;
+        this.baseTriggers[1].handler = function () {
             if (!this.readOnly && !this.disabled) {
                 var today = new Date();
                 this.setValue(today);
                 this.fireEvent('select', this, today);
             }
-		};
-		this.baseTriggers[1].hide = this.hideTriggerToday;
-	},
-	onBlur : function(){
-	/*
-	   В суперклассе Ext.form.TriggerField данный метод перекрывается пустой функцией,
-   	   видимо для того, чтобы все изменения и событие change происходили только при нажатии на триггеры,
- 	   но данное поведение весьма неудобно в колоночных фильтрах, где требуется корректное срабатывание
-           blur и change при потере фокуса.
-	   Данная реализация метода взята из базового класса Ext.formField
-	*/
-	    this.beforeBlur();
-	    if(this.focusClass){
-	        this.el.removeClass(this.focusClass);
-	    }
-	    this.hasFocus = false;
-	    if(this.validationEvent !== false && (this.validateOnBlur || this.validationEvent == 'blur')){
-	        this.validate();
-	    }
-	    var v = this.getValue();
-	    if(String(v) !== String(this.startValue)){
-	        this.fireEvent('change', this, v, this.startValue);
-	    }
-	    this.fireEvent('blur', this);
-	    this.postBlur();
+        };
+        this.baseTriggers[1].hide = this.hideTriggerToday;
+    },
+    onBlur: function () {
+        /*
+         В суперклассе Ext.form.TriggerField данный метод перекрывается пустой функцией,
+         видимо для того, чтобы все изменения и событие change происходили только при нажатии на триггеры,
+         но данное поведение весьма неудобно в колоночных фильтрах, где требуется корректное срабатывание
+         blur и change при потере фокуса.
+         Данная реализация метода взята из базового класса Ext.formField
+         */
+        this.beforeBlur();
+        if (this.focusClass) {
+            this.el.removeClass(this.focusClass);
+        }
+        this.hasFocus = false;
+        if (this.validationEvent !== false && (this.validateOnBlur || this.validationEvent == 'blur')) {
+            this.validate();
+        }
+        var v = this.getValue();
+        if (String(v) !== String(this.startValue)) {
+            this.fireEvent('change', this, v, this.startValue);
+        }
+        this.fireEvent('blur', this);
+        this.postBlur();
     }
 
 });
 
-Ext.reg('m3-date', Ext.m3.AdvancedDataField );
+Ext.reg('m3-date', Ext.m3.AdvancedDataField);
--- src/m3_ext/ui/fields/simple.py
+++ src/m3_ext/ui/fields/simple.py
@@ -7,8 +7,8 @@ Created on 27.02.2010
 """
 
 from django.conf import settings
+from django.utils.html import escapejs
 
-from m3 import date2str
 from base import BaseExtField, BaseExtTriggerField
 
 
@@ -30,17 +30,20 @@ class ExtStringField(BaseExtField):
     # def t_render_mask_re(self):
     #     return '/%s/' % self.mask_re
 
-    _xtype = "textfield"
+    _xtype = 'textfield'
 
     js_attrs = BaseExtField.js_attrs.extend(
+
+
+        enable_key_events='enableKeyEvents',  # Разрешает перехват нажатий клавиш
         input_type='inputType',
-        mask_re='maskRe',
+        mask_re='maskRe',  # маска ввода, например "(###)###-##-##"
         select_on_focus='selectOnFocus',
-        enable_key_events='enableKeyEvents'
+        input_mask='inputMask',
+
     )
 
     def __init__(self, *args, **kwargs):
-
         super(ExtStringField, self).__init__(*args, **kwargs)
         self.setdefault('enable_key_events', False)
 
@@ -49,197 +52,142 @@ class ExtDateField(BaseExtField):
     """
     Поле ввода даты
     """
-    _xtype = "datefield"
+
+    # FIXME: Переписать в js-е функцию date2str
+    # Вызывалось в render_base_config
+    #         if isinstance(self.value, datetime) or isinstance(self.value, date):
+    #         value = date2str(self.value)
+    #     else:
+    #         value = self.value
+
+    _xtype = 'm3-date'
 
     js_attrs = BaseExtField.js_attrs.extend(
-        'editable', # Признак возможности редактирования,
-        'format',
-        start_day="startDay", # атрибут задает с какого дня начинается неделя в календаре.
-                    # 0-Воскресенье, 1-Понедельник, 2-Вторник и т.д.
-        hide_today_btn="hideTodayBtn",  # Прятать кнопку "Сегодняшняя дата"
-        enable_key_events="enableKeyEvents", # Разрешает перехват нажатий клавиш
-        max_value="maxValue", # Максимальная возможная дата
-        min_value="minValue" # Минимально возможная дата
+        'format',  # Формат даты
+        'editable',  # Признак возможности редактирования
+
+        # Атрибут задает с какого дня начинается неделя в календаре.
+        # 0-Воскресенье, 1-Понедельник, 2-Вторник и т.д.
+        start_day='startDay',
+
+        hide_today_btn='hideTriggerToday',  # Прятать кнопку "Сегодняшняя дата"
+        enable_key_events='enableKeyEvents',  # Разрешает перехват нажатий клавиш
+        max_value='maxValue',  # Максимальная возможная дата
+        min_value='min_value',  # Минимально возможная дата
+
     )
-
-    def get_format(self):
-
-        default_format = 'd.m.Y'
-
-        try:
-            format_ = settings.DATE_FORMAT.replace('%', '')
-        except:
-            format_ = default_format
-
-        return format_
 
     def __init__(self, *args, **kwargs):
         super(ExtDateField, self).__init__(*args, **kwargs)
-
         self.setdefault('start_day', 1)
         self.setdefault('hide_today_btn', False)
         self.setdefault('enable_key_events', False)
-        self.setdefault('max_value', None)
-        self.setdefault('min_value', None)
-        self.setdefault('format', self.get_format())
-        self.setdefault('editable', True)
+
+        try:
+            self.setdefault('format', settings.DATE_FORMAT.replace('%', ''))
+        except:
+            self.setdefault('format', 'd.m.Y')
 
 
 class ExtNumberField(BaseExtField):
     """
     Поле ввода числового значения
     """
-    _xtype = "numberfield"
+
+    _xtype = 'numberfield'
 
     js_attrs = BaseExtField.js_attrs.extend(
-        decimal_separator="decimalSeparator",
-        allow_decimals="allowDecimals",
-        allow_negative="allowNegative",
-        decimal_precision="decimalPrecision",
-        max_value="maxValue",
-        max_text="maxText",
-        min_value="minValue",
-        min_text="minText",
-        enable_key_events="enableKeyEvents",
-        select_on_focus="selectOnFocus"
+
+        decimal_separator='decimalSeparator',  # Разделитель целой и дробной части
+        allow_decimals='allowDecimals',  # Признак, что может быть дробная часть
+        allow_negative='allowNegative',  # Признак, что могут быть отрицательные значения
+        decimal_precision='decimalPrecision',  # Точность дробной части
+        max_value='maxValue',  # Маскимально возможное значение
+        max_text='maxText',  # Если превышено максимально возможное значение, то будет отображаться этот текстs
+        min_value='minValue',  # Минимальное возможное значение
+        min_text='minText',  # Если превышено минимальное возможное значение, то будет отображаться этот текст
+        enable_key_events='enableKeyEvents',  # Разрешает перехват нажатий клавиш
+        select_on_focus='selectOnFocus',  # Выделение всего содержимого при попадании в фокус
+
     )
 
+    def __init__(self, *args, **kwargs):
+        super(ExtNumberField, self).__init__(*args, **kwargs)
+        self.setdefault('allow_decimals', True)
+        self.setdefault('allow_negative', True)
+        self.setdefault('enable_key_events', False)
 
-#===============================================================================
+
 class ExtHiddenField(BaseExtField):
     """
     Скрытое поле, которое не видно пользователю на форме, но хранит значение
     и передает его при submit'е"""
 
-    # Тип поля - integer
-    INT = 0
-    # Тип поля - string
-    STRING = 1
-
-    _xtype = "hidden"
-
-    def __init__(self, *args, **kwargs):
-        super(ExtHiddenField, self).__init__(*args, **kwargs)
-
-        # TODO: Необходимо отрефакторить под внутриклассовый рендеринг
-        self.setdefault('template', 'ext-fields/ext-hidden-field.js')
-
-        # Тип поля
-        self.setdefault('type', ExtHiddenField.INT)
+    _xtype = 'hidden'
 
 
 class ExtTextArea(BaseExtField):
     """
     Большое :) Текстовое поле
     """
+
+    # FIXME: Экранирование значений нужно делать в json.dumps
+    #
+    #  def render_base_config(self):
+    #     if self.value:
+    #         self.value = escapejs(self.value)
+    #     super(ExtTextArea, self).render_base_config()
+
     _xtype = 'textarea'
 
     js_attrs = BaseExtField.js_attrs.extend(
-        auto_create="autoCreate",
-        input_type='inputType',
-        mask_re='maskRe',
-        select_on_focus='selectOnFocus',
-        enable_key_events='enableKeyEvents'
+
+        mask_re='maskRe',  # Фильтр допустимых символов по регекспу
+        auto_create='autoCreate',  # DOM-атрибуты
     )
 
     def __init__(self, *args, **kwargs):
         super(ExtTextArea, self).__init__(*args, **kwargs)
 
-        # Фильтр допустимых символов по регекспу
-        self.setdefault('mask_re', None)
-
-        # DOM-атрибуты
-        self.setdefault('auto_create',
-            {"tag": "textarea", "autocomplete": "off"}
-        )
-#FIXME
-#    def t_render_mask_re(self):
-#        return '/%s/' % self.mask_re
-#FIXME
-#    def render_base_config(self):
-#        if self.value:
-#            self.value = escapejs(self.value)
-#        super(ExtTextArea, self).render_base_config()
-#        self._put_config_value('maskRe', self.t_render_mask_re, self.mask_re)
+        # Пусть пока это строчка будет закомиченна, так как в предыдущей версии
+        # рендеринга autoCreate вообще не рендерилось!
+        # Но! возможно какой-то прикладной код завязан как-то на эту конструкцию, что странно
+        # FIXME: Проверить, есть ли прикладной код, завязанный на auto_create
+        #self.setdefault('auto_create', {"tag": "textarea", "autocomplete": "off"})
 
 
 class ExtCheckBox(BaseExtField):
     """
     Галочка выбора значения
     """
-    _xtype = "checkbox"
+
+    _xtype = 'checkbox'
 
     js_attrs = BaseExtField.js_attrs.extend(
-        'template', 'checked',
-        box_label="boxLabel"
+        'checked',  # Признак того, что значение выбрано
+
+        box_label='boxLabel',  # Текст рядом с полем выбора значения
+
     )
-
-    def __init__(self, *args, **kwargs):
-        super(ExtCheckBox, self).__init__(*args, **kwargs)
-
-        self.setdefault('template', 'ext-fields/ext-checkbox.js')
-        self.setdefault('checked', False)
-        self.setdefault('box_label', None)
-        self._config['autoCreate'] = None
-
-#        #TODO: Необходимо отрефакторить под внутриклассовый рендеринг
-#        self.template = 'ext-fields/ext-checkbox.js'
-#
-#        # Признак того, что значение выбрано
-#        self.checked = False
-#
-#        # Текст рядом с полем выбора значения
-#        self.box_label = None
-
-#        self.init_component(*args, **kwargs)
-
-#    @property
-#    def handler_check(self):
-#        return self._listeners.get('check')
-#
-#    @handler_check.setter
-#    def handler_check(self, function):
-#        self._listeners['check'] = function
-#
-#    def _make_read_only(self, access_off=True, *args, **kwargs):
-#        self.read_only = access_off
 
 
 class ExtRadio(BaseExtField):
     """
     Радио-поле
     """
-    _xtype="radio"
+
+    # FIXME: Был вот такой странный код, надо принять решение, что делать с ним дальше
+    # def render_base_config(self):
+    #     value = self.value
+    #     self.value = None
+    #     self._put_config_value('inputValue', value)
+
+    _xtype = 'radio'
 
     js_attrs = BaseExtField.js_attrs.extend(
-        'checked',        # Признак того, что значение выбрано
-        box_label='boxLabel'    # Текст рядом с полем выбора значения
+        'checked',
+        box_label='boxLabel',
     )
-
-    def __init__(self, *args, **kwargs):
-        super(ExtRadio, self).__init__(*args, **kwargs)
-#        self._ext_name = 'Ext.form.Radio'
-        self.setdefault('checked', False)
-        self.setdefault('box_label', None)
-
-#    def render_base_config(self):
-#        value = self.value
-#        self.value = None
-#        super(ExtRadio, self).render_base_config()
-#        if self.checked:
-#            self._put_config_value('checked', True)
-#        if self.box_label:
-#            self._put_config_value('boxLabel', self.box_label)
-#        self._put_config_value('inputValue', value)
-
-#    def render(self):
-#        try:
-#            self.render_base_config()
-#        except UnicodeDecodeError as msg:
-#            raise Exception(msg)
-#
-#        base_config = self._get_config_str()
-#        return 'new %s({%s})' % (self._ext_name, base_config)
 
 
 class ExtComboBox(BaseExtTriggerField):
@@ -247,70 +195,45 @@ class ExtComboBox(BaseExtTriggerField):
     Поле выпадающий список - combobox
     """
 
-    def __init__(self, *args, **kwargs):
-        super(ExtComboBox, self).__init__(*args, **kwargs)
-        self.init_component(*args, **kwargs)
-
-    def render(self):
-        try:
-            self.render_base_config()
-
-        except UnicodeDecodeError as msg:
-            raise Exception(msg)
-
-        base_config = self._get_config_str()
-        return 'new Ext.form.ComboBox({%s})' % base_config
+    _xtype = 'm3-combobox'
 
 
-#===============================================================================
 class ExtTimeField(BaseExtField):
     """
     Поле ввода времени
     """
-    _xtype = "timefield"
+
+    _xtype = 'timefield'
 
     js_attrs = BaseExtField.js_attrs.extend(
-        # Формат отображения времени
-        'format',
-        # Шаг повышения времени
-        'increment',
-        # max и min допустимые значения времени. Задаются только в виде строки,
-        # т.к. форматы времени в python'e и javascript'e разные
-        max_value='maxValue',
-        min_value='minValue'
+        'format',  # Формат отображения времени
+        'increment',  # Шаг повышения времени
+
+        max_value='maxValue',  # max и min допустимые значения времени. Задаются только в виде строки,
+        min_value='minValue',  # т.к. форматы времени в python'e и javascript'e разные
     )
-
-    def __init__(self, *args, **kwargs):
-
-        super(ExtTimeField, self).__init__(*args, **kwargs)
-        self.setdefault('format', 'H:i:s')
-
-        self.setdefault('increment', 15)
-        self.setdefault('min_value', None)
-        self.setdefault('max_value', None)
 
 
 class ExtHTMLEditor(BaseExtField):
     """
     Поле HTML-редактор
     """
-    _xtype="htmleditor"
+
+    _xtype = 'htmleditor'
 
 
 class ExtDisplayField(BaseExtField):
     """
     Поле отображающее значение (не проверяется и не сабмитится)
     """
-    _xtype = "displayfield"
 
-    #FIXME - доделать и протестировать
-#    def render_base_config(self):
-#        super(ExtDisplayField, self).render_base_config()
-#
-#        # Для данного класса установка дополнительно css класса при read_only -
-#        # излишне
-#        if self.read_only:
-#            self._set_config_value('cls', None)
+    # FIXME: Перенести в override зависимость от read_only
+    #     # Для данного класса установка дополнительно css класса при read_only -
+    #     # излишне
+    #     if self.read_only:
+    #         self._set_config_value('cls', None)
+
+    _xtype = 'displayfield'
 
 
 class ExtDateTimeField(ExtDateField):
@@ -320,10 +243,8 @@ class ExtDateTimeField(ExtDateField):
     _xtype="datetimefield"
 
 
-
 class ExtAdvTimeField(BaseExtField):
     """
     Поле ввода времени с дополнительными ползунками
     """
     _xtype = "advtimefield"
-
