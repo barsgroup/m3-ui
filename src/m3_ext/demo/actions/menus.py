8c6295cb5eb32579e6ea07a2915ad4ef2321966c
Kirill Borisov <borisov@bars-open.ru>
2014-04-07 16:02:33 +0400

    Демонстрация присвоения обработчиков пунктам меню.

--- src/m3_ext/demo/actions/menus.py
+++ src/m3_ext/demo/actions/menus.py
@@ -12,8 +12,25 @@ class MenuAction(UIAction):
     title = u'Окно с меню'
 
     def get_js(self, request, context):
-        return """function(win, data){
-            //debugger;
+        return u"""function(win, data){
+            var first_menu = win.getComponent("panel").toolbars[0].getComponent("first-menu").menu;
+            var ro_menu = win.getComponent("panel").toolbars[0].getComponent("ro-menu").menu;
+
+            var item = first_menu.getComponent("item");
+            var dis = ro_menu.getComponent("disabled-item");
+            var toggle = first_menu.getComponent("toggle");
+
+            item.on('click', function(e){
+                alert('Здравствуй, мир!');
+            });
+            toggle.on('click', function(e){
+                dis.setDisabled(!dis.disabled);
+                alert('Ранее заблокированный пункт теперь ' + (dis.disabled ? 'не' : '') + 'доступен!');
+            });
+
+            dis.on('click', function(e){
+                alert('Вы не должны были видеть это сообщение.');
+            });
         }"""
 
     def get_ui(self, request, context):
@@ -22,26 +39,38 @@ class MenuAction(UIAction):
                             height=400,
                             layout='fit')
 
-        win.panel = ext.ExtPanel(header=True, title=u'Заголовок панели')
+        win.panel = ext.ExtPanel(
+            header=True,
+            title=u'Заголовок панели',
+            item_id='panel')
 
         self.test_menu = ext.ExtContextMenu()
-        self.test_menu.add_item(text=u'Пункт меню', item_id='item')
+        self.test_menu.add_item(
+            text=u'Пункт меню',
+            item_id='item')
+        self.test_menu.add_item(
+            text=u'Переключатель доступности',
+            item_id='toggle')
 
         self.ro_menu = ext.ExtContextMenu()
-        self.ro_menu.add_item(text=u'Недоступный пункт меню')
+        self.ro_menu.add_item(text=u'Недоступный пункт меню', item_id='disabled-item')
 
         tbar = ext.ExtToolBar()
         tbar.add_text_item(u'Текст на топбаре')
-        
+
         tbar.add_spacer(30)
-        tbar.add_menu(text=u'Меню',
-                      menu=self.test_menu,
-                      tooltip_text=u'Привет, мир!')
-        
+        tbar.add_menu(
+            text=u'Меню',
+            menu=self.test_menu,
+            tooltip_text=u'Привет, мир!',
+            item_id='first-menu')
+
         tbar.add_spacer(60)
-        ro_menu = tbar.add_menu(text=u'Ещё одно меню',
-                      menu=self.ro_menu,
-                      tooltip_text=u'Увы :(')
+        ro_menu = tbar.add_menu(
+            text=u'Ещё одно меню',
+            menu=self.ro_menu,
+            tooltip_text=u'Увы :(',
+            item_id='ro-menu')
         ro_menu.make_read_only()
 
         win.panel.top_bar = tbar
