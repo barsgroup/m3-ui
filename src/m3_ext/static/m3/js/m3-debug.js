5bea60c1e02c111dc702cb3c5d6325b6801afb1e
Telepenin Nikolay <telepenin@bars-open.ru>
2014-04-09 17:44:09 +0400

    build js

--- src/m3_ext/static/m3/js/m3-debug.js
+++ src/m3_ext/static/m3/js/m3-debug.js
@@ -3117,6 +3117,7 @@ Ext.ux.Mask = function(mask) {
     };
     Ext.apply(this, config);
 };
+
 Ext.extend(Ext.ux.Mask, Object, {
     init: function(c) {
         this.LetrasL = 'abcdefghijklmnopqrstuvwxyz';
@@ -4074,15 +4075,48 @@ Ext.reg('m3-combobox', Ext.m3.ComboBox);
  * @param {Object} config
  */
 Ext.m3.GridPanel = Ext.extend(Ext.grid.GridPanel, {
-	constructor: function(baseConfig, params){
-        params = baseConfig.params || params || {};
-		
+    initComponent: function(){
+        // Настройка грида по расширенному конфигу из параметров
+        var params = this.params || {};
+
+        // Создание ColumnModel если надо
+        var colModel = this.cm;
+        if (colModel) {
+            // раньше был экземпляр ColModel, теперь приходи конфиг
+            if (!(colModel instanceof Ext.grid.ColumnModel)) {
+                colModel = Ext.create(colModel);
+            }
+            delete this.cm;
+            this.colModel = colModel;
+        }
+
 		// Добавлене selection model если нужно
-		var selModel = params.selModel;
-		var gridColumns = params.colModel || [];
-		if (selModel && selModel instanceof Ext.grid.CheckboxSelectionModel) {
-			gridColumns.columns.unshift(selModel);
+		var selModel = this.sm;
+		if (selModel) {
+            // раньше был экземпляр SelModel, теперь приходи конфиг
+            if (!(selModel instanceof Ext.grid.AbstractSelectionModel)) {
+                selModel = Ext.create(selModel);
+            }
+            // если это чекбоксы, то добавим колонку
+            // FIXME: пока закомментировал, т.к. непонятно почему надо так делать
+            //if (selModel instanceof Ext.grid.CheckboxSelectionModel) {
+        	//    gridColumns.columns.unshift(selModel);
+            //}
+            delete this.sm;
+            this.sm = selModel;
 		}
+
+        // Создание GridView если надо
+        var view = this.view;
+        if (view) {
+            // раньше был экземпляр GridView, теперь приходи конфиг
+            if (!(view instanceof Ext.grid.GridView)) {
+                view = Ext.create(view);
+            }
+            delete this.view;
+            this.view = view;
+        }
+
 		// Навешивание обработчиков на контекстное меню если нужно
         var funcContMenu;
 		if (params.menus && params.menus.contextMenu) {
@@ -4098,7 +4132,7 @@ Ext.m3.GridPanel = Ext.extend(Ext.grid.GridPanel, {
 		} else {
             funcContMenu = Ext.emptyFn;
 		}
-		
+
 		var funcRowContMenu;
 		if (params.menus && params.menus.rowContextMenu) {
             // раньше был экземпляр меню, теперь приходи конфиг
@@ -4116,46 +4150,34 @@ Ext.m3.GridPanel = Ext.extend(Ext.grid.GridPanel, {
 		} else {
 			funcRowContMenu = Ext.emptyFn;
 		}
-		
+
 		var plugins = params.plugins || [];
 		var bundedColumns = params.bundedColumns;
 		if (bundedColumns && bundedColumns instanceof Array &&
 			bundedColumns.length > 0) {
 
-			plugins.push( 
+			plugins.push(
 				new Ext.ux.grid.ColumnHeaderGroup({
 					rows: bundedColumns
 				})
 			);
 		}
-		
-		// объединение обработчиков
-		baseConfig.listeners = Ext.applyIf({
-			contextmenu: funcContMenu
-			,rowcontextmenu: funcRowContMenu
-			,beforerender: function(){
-				var bbar = this.getBottomToolbar();
-				if (bbar && bbar instanceof Ext.PagingToolbar){
-					var store = this.getStore();
-					store.setBaseParam('start',0);
-					store.setBaseParam('limit',bbar.pageSize);
-					bbar.bind(store);
-				}
-			}	
-		}
-		,baseConfig.listeners || {});
 
-		var config = Ext.applyIf({
-			sm: selModel
-			,colModel: gridColumns
-			,plugins: plugins
-		}, baseConfig);
-		
-		Ext.m3.GridPanel.superclass.constructor.call(this, config);
-	}
-	,initComponent: function(){
-		Ext.m3.GridPanel.superclass.initComponent.call(this);
-		var store = this.getStore();
+		// объединение обработчиков
+		this.on('contextmenu', funcContMenu);
+        this.on('rowcontextmenu', funcRowContMenu);
+        this.on('beforerender', function() {
+			var bbar = this.getBottomToolbar();
+			if (bbar && bbar instanceof Ext.PagingToolbar){
+			    var store = this.getStore();
+				store.setBaseParam('start',0);
+				store.setBaseParam('limit',bbar.pageSize);
+				bbar.bind(store);
+			}
+		});
+
+        Ext.m3.GridPanel.superclass.initComponent.call(this);
+        var store = this.getStore();
 		store.on('exception', this.storeException, this);
 	}
 	/**
@@ -4163,22 +4185,65 @@ Ext.m3.GridPanel = Ext.extend(Ext.grid.GridPanel, {
 	 */
 	,storeException: function (proxy, type, action, options, response, arg){
 		//console.log(proxy, type, action, options, response, arg);
-		uiAjaxFailMessage(response, options);
+		if (type == 'remote' && action != Ext.data.Api.actions.read) {
+		    if (response.raw.message) {
+  		        Ext.Msg.show({
+  		            title: 'Внимание!',
+  		            msg: response.raw.message,
+  		            buttons: Ext.Msg.CANCEL,
+  		            icon: Ext.Msg.WARNING
+  		        });
+  		    }
+		} else {
+		    uiAjaxFailMessage(response, options);
+		}
 	}
 });
 
 Ext.m3.EditorGridPanel = Ext.extend(Ext.grid.EditorGridPanel, {
-    constructor: function(baseConfig, params){
-        params = baseConfig.params || params || {};
-    
-        // Добавлене selection model если нужно
-        var selModel = params.selModel;
-        var gridColumns = params.colModel || [];
-        if (selModel && selModel instanceof Ext.grid.CheckboxSelectionModel) {
-            gridColumns.columns.unshift(selModel);
+    initComponent: function(){
+        // Настройка грида по расширенному конфигу из параметров
+        var params = this.params || {};
+
+        // Создание ColumnModel если надо
+        var colModel = this.cm;
+        if (colModel) {
+            // раньше был экземпляр ColModel, теперь приходи конфиг
+            if (!(colModel instanceof Ext.grid.ColumnModel)) {
+                colModel = Ext.create(colModel);
+            }
+            delete this.cm;
+            this.colModel = colModel;
         }
-    
-        // Навешивание обработчиков на контекстное меню если нужно
+
+		// Добавлене selection model если нужно
+		var selModel = this.sm;
+		if (selModel) {
+            // раньше был экземпляр SelModel, теперь приходи конфиг
+            if (!(selModel instanceof Ext.grid.AbstractSelectionModel)) {
+                selModel = Ext.create(selModel);
+            }
+            // если это чекбоксы, то добавим колонку
+            // FIXME: пока закомментировал, т.к. непонятно почему надо так делать
+            //if (selModel instanceof Ext.grid.CheckboxSelectionModel) {
+        	//    gridColumns.columns.unshift(selModel);
+            //}
+            delete this.sm;
+            this.sm = selModel;
+		}
+
+        // Создание GridView если надо
+        var view = this.view;
+        if (view) {
+            // раньше был экземпляр GridView, теперь приходи конфиг
+            if (!(view instanceof Ext.grid.GridView)) {
+                view = Ext.create(view);
+            }
+            delete this.view;
+            this.view = view;
+        }
+
+		// Навешивание обработчиков на контекстное меню если нужно
         var funcContMenu;
 		if (params.menus && params.menus.contextMenu) {
             // раньше был экземпляр меню, теперь приходи конфиг
@@ -4211,47 +4276,34 @@ Ext.m3.EditorGridPanel = Ext.extend(Ext.grid.EditorGridPanel, {
 		} else {
 			funcRowContMenu = Ext.emptyFn;
 		}
-    
-        var plugins = params.plugins || [];
-        var bundedColumns = params.bundedColumns;
-        if (bundedColumns && bundedColumns instanceof Array &&
-            bundedColumns.length > 0) {
 
-            plugins.push(
-                new Ext.ux.grid.ColumnHeaderGroup({
-                    rows: bundedColumns
-                })
-            );
-        }
-    
-        // объединение обработчиков
-        baseConfig.listeners = Ext.applyIf({
-            contextmenu: funcContMenu
-            ,rowcontextmenu: funcRowContMenu
-            ,beforerender: function() {
-                var bbar = this.getBottomToolbar();
-                if (bbar && bbar instanceof Ext.PagingToolbar) {
-                    var store = this.getStore();
-                    // Оставлено, так как разработчик может поменять pageSize и новое значение
-                    // может быть не равно limit-у.
-                    store.setBaseParam('limit',bbar.pageSize);
-                    bbar.bind(store);
-                }
-            }
-        }
-        ,baseConfig.listeners || {});
+		var plugins = params.plugins || [];
+		var bundedColumns = params.bundedColumns;
+		if (bundedColumns && bundedColumns instanceof Array &&
+			bundedColumns.length > 0) {
 
-        var config = Ext.applyIf({
-            sm: selModel
-            ,colModel: gridColumns
-            ,plugins: plugins
-        }, baseConfig);
-    
-        Ext.m3.EditorGridPanel.superclass.constructor.call(this, config);
-    }
-	,initComponent: function(){
-		Ext.m3.EditorGridPanel.superclass.initComponent.call(this);
-		var store = this.getStore();
+			plugins.push(
+				new Ext.ux.grid.ColumnHeaderGroup({
+					rows: bundedColumns
+				})
+			);
+		}
+
+		// объединение обработчиков
+		this.on('contextmenu', funcContMenu);
+        this.on('rowcontextmenu', funcRowContMenu);
+        this.on('beforerender', function() {
+			var bbar = this.getBottomToolbar();
+			if (bbar && bbar instanceof Ext.PagingToolbar){
+			    var store = this.getStore();
+				store.setBaseParam('start',0);
+				store.setBaseParam('limit',bbar.pageSize);
+				bbar.bind(store);
+			}
+		});
+
+        Ext.m3.EditorGridPanel.superclass.initComponent.call(this);
+        var store = this.getStore();
 		store.on('exception', this.storeException, this);
 	}
 	/**
@@ -6984,7 +7036,6 @@ Ext.m3.AdvancedComboBox = Ext.extend(Ext.m3.ComboBox, {
         this.triggerSelectClass = 'x-form-select-trigger';
         this.triggerEditClass = 'x-form-edit-trigger';
 
-
         assert(params.actions, 'params.actions is undefined');
 
         if (params.actions.actionSelectUrl) {
@@ -6993,6 +7044,9 @@ Ext.m3.AdvancedComboBox = Ext.extend(Ext.m3.ComboBox, {
 
         if (params.actions.actionEditUrl) {
             this.actionEditUrl = params.actions.actionEditUrl;
+        }
+        if (!baseConfig.store.url && params.actions.autocompleteUrl) {
+            baseConfig.store.url = params.actions.autocompleteUrl;
         }
 
         this.askBeforeDeleting = params.askBeforeDeleting;
@@ -7007,7 +7061,7 @@ Ext.m3.AdvancedComboBox = Ext.extend(Ext.m3.ComboBox, {
 
         this.defaultValue = params.defaultValue;
         this.defaultText = params.defaultText;
-        this.defaultRecord = Ext.decode(params.recordValue);
+        this.defaultRecord = params.recordValue;
 
         this.baseTriggers = [
             {
@@ -7032,12 +7086,6 @@ Ext.m3.AdvancedComboBox = Ext.extend(Ext.m3.ComboBox, {
             }
         ];
         this.allTriggers = [].concat(this.baseTriggers);
-        if (params.customTriggers) {
-            Ext.each(params.customTriggers, function (item, index, all) {
-                this.allTriggers.push(item);
-            }, this);
-
-        }
 
         Ext.m3.AdvancedComboBox.superclass.constructor.call(this, baseConfig);
     },
