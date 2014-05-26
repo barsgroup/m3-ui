9aa73d2148841e37c4ef7794917a07048f728959
Пирогов Алексей <devnull@localhost>
2014-05-26 13:52:09 +0400

    workspace view fix

--- src/m3_ext/views.py
+++ src/m3_ext/views.py
@@ -4,6 +4,7 @@ import json
 from django.core.urlresolvers import reverse
 
 from django.http import HttpResponse
+from django.conf import settings
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
@@ -12,79 +13,78 @@ from m3_ext.ui.app_ui import BaseDesktopElement, MenuSeparator
 from context_processors import desktop_processor
 
 
-def workspace(template='m3_workspace.html'):
+def workspace(template='m3_workspace.html', context_processors=None):
     u"""
     Возвращает view для отображения Рабочего Стола
     на основе указанного шаблона
     :param template: имя файла шаблона
     :type template: str
     """
-
-    def workspace_view(request):
-        u"""
-        view для отображения Рабочего Стола
-        """
-        context = RequestContext(request, {
-            'desktop_url': reverse(desktop_items)
-        })
-        return render_to_response(template,  context)
-    return workspace_view
-
-
-def desktop_items(request, context_processors=None):
-    u"""
-    Возвращает view для отображения Рабочего Стола
-    на основе указанного шаблона
-    :param template: имя файла шаблона
-    :type template: str
-    """
-
-    def make_context(request):
+    def make_context(request, ctx):
         """
         Формирует контекст, прогоняя request через цепочку процессоров
         """
         return reduce(
             lambda x, f: x.update(f(request)) or x,
-            [desktop_processor] + list(context_processors or []),
-            {}
+            context_processors or [],
+            ctx
         )
 
-    class Encoder(json.JSONEncoder):
+    def workspace_view(request):
+        u"""
+        view для отображения Рабочего Стола
         """
-        JSONEncoder, совместимый с клиентским рендерингом
+        context = RequestContext(
+            request,
+            make_context(
+                request,
+                {'desktop_url': reverse(desktop_items)}))
+        return render_to_response(template, context)
+
+    return workspace_view
+
+
+def desktop_items(request):
+    u"""
+    Возвращает конфигурацию элементов Рабочего Стола
+    """
+
+    def default(obj):
         """
-
-        def default(self, obj):
-
-
-            """
-            Возвращает UI-элемент в сериализованном виде
-            :param obj: элемент рабочего стола
-            :type obj: MenuSeparator / BaseDesktopElement
-            """
-            if isinstance(obj, MenuSeparator):
-                return '-'
-            elif isinstance(obj, BaseDesktopElement):
-                result = {
-                    'text': obj.name,
-                    'icon': obj.icon,
-                }
-                if obj.has_subitems:
-                    result['items'] = list(obj.subitems)
+        Возвращает UI-элемент в сериализованном виде
+        :param obj: элемент рабочего стола
+        :type obj: MenuSeparator / BaseDesktopElement
+        """
+        if isinstance(obj, MenuSeparator):
+            return '-'
+        elif isinstance(obj, BaseDesktopElement):
+            result = {
+                'text': obj.name,
+                'icon': obj.icon,
+            }
+            if obj.has_subitems:
+                result['items'] = list(obj.subitems)
+            else:
+                result['url'] = obj.url
+                context = getattr(obj, 'context', {})
+                if context:
+                    result['context'] = context
                 else:
-                    result['url'] = obj.url
-                    context = getattr(obj, 'context', {})
-                    if context:
-                        result['context'] = context
-                    else:
-                        result['context'] = {}
+                    result['context'] = {}
 
-                return result
+            return result
 
-            return super(Encoder, self).default(obj)
+        return super(Encoder, self).default(obj)
 
-    # def wrapper(request):
-    return HttpResponse(Encoder().encode(make_context(request)),
-                        mimetype='text/json')
+    if settings.DEBUG:
+        opts = {
+            'indent': 4,
+            'separators': (', ', ': '),
+        }
+    else:
+        opts = {'separators': (',', ':')}
 
-    # return wrapper
+    return HttpResponse(
+        json.dumps(desktop_processor(request), default=default, **opts),
+        mimetype='text/json'
+    )
