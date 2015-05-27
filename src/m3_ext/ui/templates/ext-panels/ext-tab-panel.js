4c19a93f93beafa40b5745fd8eee169fb594977c
Pavel Antonov <pvantonov@bars-open.ru>
2015-05-27 13:43:54 +0600

    Автоматическая перекомпановка содержимого при активации таба

--- src/m3_ext/ui/templates/ext-panels/ext-tab-panel.js
+++ src/m3_ext/ui/templates/ext-panels/ext-tab-panel.js
@@ -4,6 +4,7 @@ new Ext.TabPanel({
 	{% if component.icon_cls %} ,iconCls: '{{ component.icon_cls }}' {% endif %}
 	{% if component.title %} ,title: '{{ component.title }}' {% endif %}
     {% if component.deferred_render != None %},deferredRender: {{ component.deferred_render|lower }} {% endif %}
+    {% if component.layout_on_tab_change != None %},layoutOnTabChange: {{ component.layout_on_tab_change|lower }} {% endif %}
     ,activeTab: {% if component.active_tab %}{{ component.active_tab }}{% else %}0{% endif %}
 	{% if component.auto_width %} ,autoWidth: true {% endif %}
 	,tabPosition: '{{ component.tab_position }}'
