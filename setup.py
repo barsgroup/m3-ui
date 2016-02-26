2aa60aa4028637d7e7f218f7ebc914f578998caa
Dmitriy Chernyshov <dechernyshov@bars-open.ru>
2016-02-26 08:44:41 +0300

    2.0.7.17

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='m3-ui',
-    version='2.0.7.16',
+    version='2.0.7.17',
     url='https://bitbucket.org/barsgroup/m3-ext',
     license='MIT',
     author='BARS Group',
