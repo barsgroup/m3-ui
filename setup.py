2299ed6609b2d31e6d3a16a26f318cf344972a33
Akhmadiev Timur <xyzza@MacBook-Pro-Akhmadiev.local>
2016-03-30 15:14:12 +0300

    2.0.7.18

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='m3-ui',
-    version='2.0.7.17',
+    version='2.0.7.18',
     url='https://bitbucket.org/barsgroup/m3-ext',
     license='MIT',
     author='BARS Group',
