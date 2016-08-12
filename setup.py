8b7928f83c65490fa37118cdcd68574c31f67c1d
Андрей Чагочкин <andrey.chagochkin@bars-open.ru>
2016-08-12 11:14:43 +0600

    EDUM3-14 Подключение m3-builder.

--- setup.py
+++ setup.py
@@ -1,29 +1,36 @@
 # coding: utf-8
 import os
 
+from pip.download import PipSession
+from pip.req.req_file import parse_requirements
 from setuptools import setup, find_packages
 
 
-def read(fname):
+def _get_requirements(file_name):
+    pip_session = PipSession()
+    requirements = parse_requirements(file_name, session=pip_session)
+
+    return tuple(str(requirement.req) for requirement in requirements)
+
+
+def _read(fname):
     try:
-        return open(os.path.join(os.path.dirname(__file__),
-                                 fname)).read()
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
     except IOError:
         return ''
 
 
 setup(
     name='m3-ui',
-    version='2.1.4',
     url='https://bitbucket.org/barsgroup/m3-ext',
     license='MIT',
     author='BARS Group',
     author_email='bars@bars-open.ru',
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    description=read('DESCRIPTION'),
-    install_requires=read('REQUIREMENTS'),
-    long_description=read('README'),
+    description=_read('DESCRIPTION'),
+    install_requires=_get_requirements('REQUIREMENTS'),
+    long_description=_read('README'),
     include_package_data=True,
     classifiers=[
         'Intended Audience :: Developers',
@@ -35,4 +42,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Development Status :: 5 - Production/Stable',
     ],
+    dependency_links=(
+        'http://pypi.bars-open.ru/simple/m3-builder',
+    ),
+    setup_requires=(
+        'm3-builder>=1.0.1',
+    ),
+    set_build_info=os.path.dirname(__file__),
 )
