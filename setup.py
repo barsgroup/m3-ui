4e8ffd16f47bc91e69f2c6aabdf26ff869148540
Тимур Ахмадиев <akhmadiev@bars-open.ru>
2016-12-23 15:28:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='m3-ui',
-    version='2.0.7.27',
+    version='2.0.7.28',
     url='https://bitbucket.org/barsgroup/m3-ext',
     license='MIT',
     author='BARS Group',
